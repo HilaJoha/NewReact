7031752e869f0892026925c02e43cd7a4fd37301
Johanna Hilaire <johannahilairepro@gmail.com>
2023-05-31 11:03:23 +0200

    Update PokemonCard component with Mew character

---
+++ my-new-app/src/Components/PokemonCard.jsx
@@ -0,0 +1,27 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+  
+function PokemonCard() {
+  const pokemon = pokemonList[1];
+
+    return (
+      <figure>
+        {pokemon.imgSrc != null ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>????</p>} <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+  }
+   
+  export default PokemonCard;
+
+  
+
+  
