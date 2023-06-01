e87d9a3d3c12c0dc93ea309655918f19942a34e3
Johanna Hilaire <johannahilairepro@gmail.com>
2023-06-01 17:06:57 +0200

    alert messages displaying on page load and when the pokemon card shows

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -1,11 +1,19 @@
-// App.jsx
-
+import { useEffect } from "react";
 import React, { useState } from 'react';
 import PokemonCard from './components/PokemonCard';
 import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
+
+  useEffect(
+    () => {
+      alert("hello pokemon trainer :)");
+    },
+    []
+  );
+
+
   const pokemonList = [
     {
       name: "bulbasaur",
@@ -28,31 +36,40 @@ function App() {
     },
   ];
 
-  const [currentIndex, setCurrentIndex] = useState(0);
+   const [currentIndex, setCurrentIndex] = useState(0);
 
-  
   function handlePrevious() {
-    setCurrentIndex(function(prevIndex) {
-      return prevIndex === 0 ? pokemonList.length - 1 : prevIndex - 1;
-    });
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? pokemonList.length - 1 : prevIndex - 1));
+    window.alert('Hello Pokemon Trainer!');
   }
 
   function handleNext() {
-    setCurrentIndex(function(prevIndex) {
-      return prevIndex === pokemonList.length - 1 ? 0 : prevIndex + 1;
-    });
+    setCurrentIndex((prevIndex) => (prevIndex === pokemonList.length - 1 ? 0 : prevIndex + 1));
+    if (pokemonList[currentIndex].name === 'pikachu') {
+      window.alert('Pika Pikachu!!!');
+    }
   }
+
+  useEffect(() => {
+    if (pokemonList[currentIndex].name === 'pikachu') {
+      const delay = 100; // Délai de 0,1 secondes (100 millisecondes)
+      const timeoutId = setTimeout(() => {
+        alert('pika pikachu !!!');
+      }, delay);
+
+      return () => {
+        clearTimeout(timeoutId); // Nettoie le timeout lorsque le composant est démonté ou lorsque l'index change
+      };
+    }
+  }, [currentIndex]);
+
 
   return (
     <div>
       <PokemonCard pokemon={pokemonList[currentIndex]} />
       <NavBar handlePrevious={handlePrevious} handleNext={handleNext} />
-      
     </div>
   );
 }
 
 export default App;
-
-
-
